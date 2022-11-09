e1405d8252086ea5699399097a87978cd5085d99
leo_name <leo@mail>
2022-11-09 23:16:51 +0200

    Third string was added

--- HellowWorld.c
+++ HellowWorld.c
@@ -5,9 +5,7 @@ int main(){
     printf("Hellow World\n\n");
 
     printf("First string\n\n");
-    printf("Second string string\n\n");
-
-
-
+    printf("Second string\n\n");
+    printf("Third string\n\n");
     printf("------------------------\n");
 }
